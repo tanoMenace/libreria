33f7d3b1dcfcaeba8b4c13d8d28cfb950d582199
tanoMenace <104911140+tanoMenace@users.noreply.github.com>
2022-05-04 19:18:12 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,9 @@
-//struttura generale del codice
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
+
 using namespace std;
 
 int main()
 {
-    string codice, titolo, autore, editore, anno, prezzo, tag;
+    cout << "Hello world!" << endl;
     return 0;
-}
-void inserisci(string codice, string titolo, string autore, string editore, string anno, string prezzo, string tag)
-{
-
-}
-void catalogo()
-{
-
-}
-void cancella(string codice)
-{
-
-}
-void ricerca(string codice)
-{
-
-}
-void modifica(string codice, string titolo, string autore, string editore, string anno, string prezzo, string tag)
-{
-
 }
