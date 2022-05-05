e92b63f55ac850edda97717a8a2c3a84d35e277a
tanoMenace <104911140+tanoMenace@users.noreply.github.com>
2022-05-06 00:07:44 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -56,19 +56,110 @@ libreria catalogo(libreria vetl, int evo)
         cout<<cat[i].codice<<"   "<<cat[i].titolo<<"/"<<cat[i].autore<<"/"<<cat[i].editore<<"/"<<cat[i].anno<<"/"<<cat[i].prezzo<<"/"<<cat[i].tag;
         cout<<endl;
     }
-    return ;
+    return cat;
 }
 void cancella(int evo)
 {
+    string cod_tempo;
+    int sco;
 
+    catalogo(vetl, n);
+
+    cout<<"Inserire codice ISBN del libro da eliminare dal catalogo: ";
+    cin>>cod_tempo;
+
+    sco=ricercaCodice(libri, n, codice);
+
+    if(cod_tempo!=-1)
+    {
+        for(int i=cod_tempo; i<n-1; i++)
+        {
+            vetl[i]=vetl[i+1];
+        }
+        n--;
+    }
+    else{
+        cout<<"Codice ISBN non trovato";
+        cout<<endl;
+    }
 }
 void ricerca(int evo)
 {
+    string cod_tempo;
+    int sco;
+
+    cout<<"Inserire codice ISBN del libro da trovare: ";
+    cin>>cod_tempo;
+
+    for(int i = 0; i<n; i++)
+    {
+        if(cod_tempo==vetl[i].titolo)
+        {
+            sco=i;
+            if(indice != -1)
+            {
+                cout<<cat[sco].codice<<"   "<<cat[sco].titolo<<"/"<<cat[sco].autore<<"/"<<cat[sco].editore<<"/"<<cat[sco].anno<<"/"<<cat[sco].prezzo<<"/"<<cat[sco].tag;
+                cout<<endl;
+            }
+                else
+                {
+                    cout<<"Codice ISBN non trovato";
+                    cout<<endl;
+                }
+        }
+    }
+
 
 }
 void modifica(int evo)
 {
+    string cod_tempo;
+    int sco;
 
+    libreria inserire;
+    catalogo(vetl, n);
+
+    cout<<"Inserire codice ISBN del libro da modificare: ";
+    cin>>cod_tempo;
+
+    for(int i = 0; i<n; i++)
+    {
+        if(cod_tempo==vetl[i].titolo)
+        {
+            sco=i;
+            if(indice != -1)
+            {
+                cout<<"Inserire il codice ISBN del libro: ";
+                    cin>>inserire.codice;
+
+                    cout<<"Inserire il titolo del libro: ";
+                    cin>>inserire.titolo;
+
+                    cout<<"Inserire l' autore del libro: ";
+                    cin>>inserire.autore;
+
+                    cout<<"Inserire l' editore del libro: ";
+                    cin>>inserire.editore;
+
+                    cout<<"Inserire l' anno di pubblicazione del libro: ";
+                    cin>>inserire.anno;
+
+                    cout<<"Inserire il prezzo del libro: ";
+                    cin>>inserire.prezzo;
+
+                    cout<<"Inserire il tag del genere letterario: ";
+                    cin>>inserire.tag;
+
+                    cout<<endl;
+                    cout<<endl;
+            }
+                    else
+                {
+                    cout<<"Codice ISBN non trovato";
+                    cout<<endl;
+                }
+        }
+    }
 }
 void menu()
 {
